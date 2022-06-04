760587d2fcfe963d1057c6c09ae6417e0845714d
Nico-Werner <nicolas.werner@ing.austral.edu.ar>
2022-06-04 16:14:31 -0300

    First commit

---
+++ src/main/java/com/post/jibberjabberposts/JibberJabberPostsApplication.java
@@ -0,0 +1,13 @@
+package com.post.jibberjabberposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/jibberjabberposts/JibberJabberPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.jibberjabberposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
