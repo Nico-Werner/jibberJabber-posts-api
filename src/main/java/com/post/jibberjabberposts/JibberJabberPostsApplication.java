40794d6413262f109d92770579e0ee544663aadc
Nico-Werner <nicolas.werner@ing.austral.edu.ar>
2022-06-12 00:46:58 -0300

    Small changesv2

--- src/main/java/com/post/jibberjabberposts/JibberJabberPostsApplication.java
+++ src/main/java/com/post/jibberjabberposts/JibberJabberPostsApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JibberJabberPostsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JibberJabberPostsApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(JibberJabberPostsApplication.class, args); }
 
 }
