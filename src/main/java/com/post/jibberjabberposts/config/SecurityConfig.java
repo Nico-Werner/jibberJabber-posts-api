2882fb9748021ebdf718153b790b8115c303b3bb
Nico-Werner <nicolas.werner@ing.austral.edu.ar>
2022-06-17 16:54:17 -0300

    removed /user from sec confug

--- src/main/java/com/post/jibberjabberposts/config/SecurityConfig.java
+++ src/main/java/com/post/jibberjabberposts/config/SecurityConfig.java
@@ -32,7 +32,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-                .antMatchers("/post/*", "/user/*")
+                .antMatchers("/post/*")
                 .hasAnyRole("jj-user", "jj-admin")
                 .anyRequest()
                 .permitAll();
